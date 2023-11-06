638963be4a28c5cbfde82b42eff65b4118d34f17
Arham Naeem <arhamn205@gmail.com>
2023-11-06 17:16:35 +0500

    made canges

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import torch
-from transformers import AutoTokenizer, AutoModel
-from sklearn.metrics.pairwise import cosine_similarity
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import re
+from transformers import AutoTokenizer, AutoModelWithLMHead
+import torch
+from nltk.tokenize import sent_tokenize
 from fastapi.middleware.cors import CORSMiddleware
+from transformers import AutoTokenizer, AutoModel
+from sklearn.metrics.pairwise import cosine_similarity
 import random
+from typing import List, Tuple
+
+
 # Define the FastAPI app
 app = FastAPI()
 
@@ -20,10 +25,97 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class KeywordScore(BaseModel):
+    keyword: str
+    score: float
+
+class RatingResponse(BaseModel):
+    similarity_score: float
+    keyword_score: List[KeywordScore]
+
 class URLInput(BaseModel):
     url: str
 
-@app.post("/rating", response_model=float)  # Change response_model to float
+class SummarizedText(BaseModel):
+    text: str
+
+@app.post("/summarize", response_model=SummarizedText)
+async def summarize(url_input: URLInput):
+    url = url_input.url
+    # Create a fake user agent
+    user_agent = UserAgent()
+
+    # Send an HTTP request to the URL with the fake user agent
+    headers = {'User-Agent': user_agent.random}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        try:
+            # Parse the HTML content of the page using BeautifulSoup
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            # Find and extract text within both <h> (header) and <p> (paragraph) tags
+            header_text = [h.get_text() for h in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])]
+            paragraph_text = [p.get_text() for p in soup.find_all('p')]
+
+            # Combine all the extracted text into a single string
+            all_text = ' '.join(header_text + paragraph_text)
+
+            # Use the regular expression to filter for Arabic text
+            arabic_text = re.findall(r'[\u0600-\u06FF\s]+', all_text)
+
+            # Join the extracted Arabic text into a single string
+            extracted_arabic_text = ' '.join(arabic_text)
+
+            # Initialize the summarization model
+            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+            m_name = "marefa-nlp/summarization-arabic-english-news"
+
+            tokenizer = AutoTokenizer.from_pretrained(m_name)
+            model = AutoModelWithLMHead.from_pretrained(m_name).to(device)
+
+            def get_summary(text, tokenizer, model, device="cpu", num_beams=2):
+                if len(text.strip()) < 50:
+                    return ["Please provide a longer text"]
+
+                text = "summarize: <paragraph> " + " <paragraph> ".join([s.strip() for s in sent_tokenize(text) if s.strip() != ""]) + " </s>"
+                text = text.strip().replace("\n", "")
+
+                tokenized_text = tokenizer.encode(text, return_tensors="pt").to(device)
+
+                summary_ids = model.generate(
+                    tokenized_text,
+                    max_length=512,
+                    num_beams=num_beams,
+                    repetition_penalty=1.5,
+                    length_penalty=1.0,
+                    early_stopping=True
+                )
+
+                output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+                return [s.strip() for s in output.split("<hl>") if s.strip() != ""]
+
+            # Initialize the final resultant text
+            final_resultant_text = ""
+
+            # Break the Arabic text into chunks of 1000 characters
+            chunk_size = 1000
+            chunks = [extracted_arabic_text[i:i + chunk_size] for i in range(0, len(extracted_arabic_text), chunk_size)]
+
+            # Generate and append each chunk's summary to the final resultant text
+            for chunk in chunks:
+                summaries = get_summary(chunk, tokenizer, model, device)
+                for summary in summaries:
+                    final_resultant_text += summary + " "
+
+            return {"text": final_resultant_text}
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+    else:
+        raise HTTPException(status_code=400, detail=f"Failed to retrieve the page. Status code: {response.status_code}")
+    
+    
+@app.post("/rating", response_model=RatingResponse)  # Change response_model to float
 async def calculate_similarity(url_input: URLInput):
     url = url_input.url
 
@@ -56,7 +148,7 @@ async def calculate_similarity(url_input: URLInput):
     # file_path = 'D:\AI - Aliasghar\Ali Asghar\Arabic Text\keywords.txt'
     file_path='keywords.txt'
     with open(file_path, 'r', encoding='utf-8') as file:
-        keywords = file.read()
+        keywords = file.read().splitlines()
 
     # Load a pre-trained Arabic BERT model and tokenizer
     model_name = "aubmindlab/bert-base-arabertv02"
@@ -75,14 +167,16 @@ async def calculate_similarity(url_input: URLInput):
     similarity = cosine_similarity(text1_embeddings, text2_embeddings)
     similarity_score = similarity[0][0]
     keyword_score = []
+    print(keywords)
     for keyword in keywords:
-        keyword_score.append([keyword,random.randint(int(similarity_score * 100), 10)])
-    response = {
-        "similarity_score": similarity_score,
-        "keyword_score": keyword_score
-    }
-    return response  # Return the similarity score
+       keyword_score.append(KeywordScore(keyword=keyword, score=random.randint(0,10)))
+       print(keyword)
+
+
+    return RatingResponse(similarity_score=similarity_score, keyword_score=keyword_score)
+
+
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=7000)
+    uvicorn.run(app, host="localhost", port=8000)
